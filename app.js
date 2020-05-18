67eed2438ad2dc63c687df78f5f8712f371d60d3
Chris W Jones <chris.jones@cbsinteractive.com>
2020-05-18 19:58:17 -0400

    Sets view engine for express
    This adds the pug library as a dependency to my project and tells
    express to use it to render any templates that I create.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 app.get(/.*/, (req, res) => {
   res.send('Hello World!');
